a34f402b80777c4fcd2168835d8b5fa3ec5a8b08
shlomi <shlomiasi1@gmail.com>
2022-12-26 13:30:10 +0200

    temp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from "react"
+import './App.css'
 
 const App = () => {
   const [users, setUsers] = useState([])
+  const [name, setName] = useState('');
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleAdd() {
+    setUsers([...users,name])
+ }
 
   const fetchData = () => {
+
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => {
         return response.json()
@@ -19,12 +30,30 @@ const App = () => {
 
   return (
     <div className="App" id="App">
+          <label for="name">User Name</label><br/>
+          <input name="name" type="text" value={name} onChange={handleChange}/><br/>
+
+          <button type="button" onClick={handleAdd} >Add User</button><br/>
+
       {users.length > 0 && (
-        <ul>
-          {users.map(user => (
-            <li id = "li" key={user.name}>{user.email}</li>
-          ))}
-        </ul>
+ <table>
+ <thead>
+     <tr>
+         <th>Name</th>
+         <th>Email</th>
+     </tr>
+ </thead>
+ <tbody>
+     {users.map((row) => {
+         return (
+             <tr>
+                 <td key = {row.name}>{row.name}</td>
+                 <td key = {row.name}>{row.email}</td>
+             </tr>
+         )
+     })}
+ </tbody>
+</table>
       )}
     </div>
   )
